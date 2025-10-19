f5f44993b2da43a67a4e3dc01e37021d55c0fc95
Ivan Gorskiy <gorskii241152003@gmail.com>
2025-10-19 13:57:05 +0300

    dsf

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default {
-  server: {
-    host: true,
-    hmr: { protocol: 'ws', host: 'localhost', port: 5173 }, // or clientPort: 5173
-  },
-}
+export default defineConfig({
+  plugins: [react()],
+})
